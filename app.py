4898981286c5eb762d968af2a219232b986f0e4c
amf5214 <austinfraley.lee@gmail.com>
2023-11-12 02:21:19 -0500

    Moved database models to models.py
    Moved database models to models.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, json, Response, jsonify, make_response, flash
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm.exc import NoResultFound
 from authentication import create_password, validate_password
 import sys
@@ -14,8 +13,7 @@ import base64
 from io import BytesIO #Converts data from Database into bytes
 from sqlalchemy import create_engine
 import pymysql
-from sqlalchemy.dialects.mysql import LONGTEXT
-
+from src.models import *
 
 logging.basicConfig(filename='record.log', level=logging.DEBUG, filemode="w")
 
@@ -26,143 +24,10 @@ with app.app_context():
     app.config["SECRET_KEY"] = "jgjdfk34benrgtgjfhbdnjmkf5784iejkdshjssefwr"
     app.config["UPLOAD_FOLDER"] = "static/uploads/"
     app.config["ITEM_FOLDER"] = "static/items/"
-    db = SQLAlchemy(app)
+
+    db.init_app(app)
+    
     logging.info("Database configured")
-
-    class FrequentlyAskedQuestion(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        author_user = db.Column(db.String(200), nullable=True)
-        question = db.Column(db.String(200), nullable=True)
-        answer = db.Column(db.String(200), default="")
-        answer_author = db.Column(db.String(200), default="")
-
-        def __repr__(self):
-                return f"<FAQ {self.id}>"
-        
-        def to_string(self):
-            return f"FAQ {self.id} by author {self.author_user}: {self.question}"
-       
-    class PageObject(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        item_title = db.Column(db.String(200), nullable=False)
-        image_link = db.Column(db.String(200))
-        item_description = db.Column(db.Text, nullable=True)
-        iframe_video_link = db.Column(db.String(500), nullable=True, default="www.google.com")
-        crafting_image_links = db.Column(db.String(200))
-        smelting_image_links = db.Column(db.String(200))
-        source_mod = db.Column(db.String(50))
-        stack_size = db.Column(db.Integer)
-        item_rarity = db.Column(db.Enum("Common", "Uncommon", "Rare", "Impossible", "Creative Only", ""))
-        dimension = db.Column(db.String(30))
-        item_type = db.Column(db.String(20))
-        minecraft_item_id = db.Column(db.String(1000))
-
-        def __repr__(self):
-            return f"<PageObj {self.id}>"
-
-    class UserAccount(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(50))
-        full_name = db.Column(db.String(100))
-        auth_account_id = db.Column(db.Integer)
-        birthdate = db.Column(db.Date)
-        account_image_link = db.Column(db.String(100))
-        bio = db.Column(db.Text)
-        experience = db.Column(db.Text)
-
-        def __repr__(self):
-            return f"<UserAccount {self.id}>"
-        
-        def set_auth(self, auth_account):
-            self.auth = auth_account
-        
-    class AccountPermission(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        permission_type = db.Column(db.String(50))
-        account_id = db.Column(db.Integer)
-        grant_date = db.Column(db.Date)
-
-        def __repr__(self):
-            return f"<AccountPermission {self.id}>"
-        
-    class PermissionsRequest(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        username = db.Column(db.String(50))
-        permission_type = db.Column(db.String(50))
-        account_id = db.Column(db.Integer)
-        grant_date = db.Column(db.Date)
-        is_visible = db.Column(db.Integer, default=1)
-
-        def __repr__(self):
-            return f"<AccountPermission Request {self.id}>"
-        
-    class AuthAccount(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        email_account = db.Column(db.String(100), unique=True)
-        hash_password = db.Column(db.String(1000))
-        auth_token = db.Column(db.String(1000))
-
-        def __repr__(self):
-            return f"<AuthAccount {self.id}>"
-
-    class FileContent(db.Model):
-
-      id = db.Column(db.Integer,  primary_key=True)
-      name = db.Column(db.String(128), nullable=False)
-      rendered_data = db.Column(db.Text(max), nullable=False) #Data to render the pic in browser
-      text = db.Column(db.Text)
-      location = db.Column(db.String(64))
-      pic_date = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
-
-      def __repr__(self):
-          return f"<FileContent {self.id}>"
-
-    class ItemClass(db.Model):
-
-        id = db.Column(db.Integer,  primary_key=True)
-        name = db.Column(db.String(128), nullable=False)
-
-        def __repr__(self):
-          return f"<ItemClass {self.id}>"
-        
-    class WebPage(db.Model):
-
-        id = db.Column(db.Integer,  primary_key=True)
-        text = db.Column(db.Text)
-        div_title = db.Column(db.String(255))
-        path = db.Column(db.String(255))
-        directory = db.Column(db.String(255))
-
-        def __repr__(self):
-          return f"<WebPage {self.id}>"
-        
-    class DivContainer(db.Model):
-
-        id = db.Column(db.Integer,  primary_key=True)
-        text = db.Column(db.Text)
-        div_title = db.Column(db.String(255))
-        display_type = db.Column(db.String(255))
-        flex_direction = db.Column(db.String(255))
-        page_id = db.Column(db.Integer)
-        placement_order = db.Column(db.Integer)
-
-        def __repr__(self):
-          return f"<DivContainer {self.id}>"
-        
-    class PageElement(db.Model):
-
-        id = db.Column(db.Integer,  primary_key=True)
-        element_type = db.Column(db.String(255))
-        div_id = db.Column(db.Integer)
-        text = db.Column(db.Text)
-        div_title = db.Column(db.String(255))
-        page_id = db.Column(db.Integer)
-        placement_order = db.Column(db.Integer)
-
-        def __repr__(self):
-          return f"<PageElement {self.id}>"
-            
-    logging.info("Table classes configured")
 
     class Permission():
         def __init__(self, has, name):
@@ -805,8 +670,8 @@ def deletewebpage(pageid):
 @app.route('/learn/<pagepath>/<editable>')
 def learningpages(pagepath, editable):
     page = db.session.execute(db.select(WebPage).filter_by(path=pagepath)).scalar_one()
-    divs = db.session.execute(db.select(DivContainer).filter_by(page_id=page.id)).order_by(DivContainer.placement_order).scalars()
-    elements = db.session.execute(db.select(PageElement).filter_by(page_id=page.id)).order_by(PageElement.div_id, PageElement.placement_order).scalars()
+    divs = db.session.execute(db.select(DivContainer).filter_by(page_id=page.id).order_by(DivContainer.placement_order)).scalars()
+    elements = db.session.execute(db.select(PageElement).filter_by(page_id=page.id).order_by(PageElement.div_id, PageElement.placement_order)).scalars()
     div_elements = {}
 
     for element in elements:
@@ -827,7 +692,7 @@ def learningpages(pagepath, editable):
             
     if editable == "true":
         if check_if_editor(request):
-            return render_template("learnpage.html", divs=divs_elements, page=page, useraccount=get_account(request), editable=True) 
+            return render_template("learnpage.html", divs=set(div_lst), page=page, useraccount=get_account(request), editable=True) 
         else:
             return redirect(f"/learn/{page.path}/false")
     else:
