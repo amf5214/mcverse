4b6edfdc8af150518572276a913edd1ee569692d
amf5214 <austinfraley.lee@gmail.com>
2023-11-22 07:43:38 -0500

    Updated logging manager
    Log files will now restart after each run. Also, improved the format to provide more data

--- src/logging_manager.py
+++ src/logging_manager.py
@@ -3,9 +3,10 @@ import logging.handlers
 import os
 
 def create_logger(filename):
-    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'))
-    formatter = logging.Formatter(logging.BASIC_FORMAT)
+    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
+    formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
     handler.setFormatter(formatter)
+
     root = logging.getLogger(filename)
     root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
     root.addHandler(handler)
@@ -13,11 +14,11 @@ def create_logger(filename):
     return root
 
 def get_root_logger(filename):
-    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'))
+    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
     formatter = logging.Formatter(logging.BASIC_FORMAT)
     handler.setFormatter(formatter)
     root = logging.getLogger()
-    root.setLevel(os.environ.get("LOGLEVEL", "INFO"))
+    root.setLevel(os.environ.get('LOGLEVEL', 'INFO'))
     root.addHandler(handler)
 
     return root
