772ead5bc95cf751150dc0ce36ae186ebb3c4cfc
amf5214 <austinfraley.lee@gmail.com>
2023-11-27 14:46:05 -0500

    Added docstring to functions that are present

--- src/logging_manager.py
+++ src/logging_manager.py
@@ -3,6 +3,15 @@ import logging.handlers
 import os
 
 def create_logger(filename):
+    """Adds a handler to a new logger at the specified file location
+
+    Takes in a filename/location and adds a logging handler to a new logger at the specified location with the specified name
+
+    Keyword Arguements:
+    filename -- string with the filename and location
+
+    Return: Logger object with handler
+    """
     handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
     formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
     handler.setFormatter(formatter)
@@ -14,6 +23,15 @@ def create_logger(filename):
     return root
 
 def get_root_logger(filename):
+    """Adds a handler to the root logger at the specified file location
+
+    Takes in a filename/location and adds a logging handler to the root logger at the specified location with the specified name
+
+    Keyword Arguements:
+    filename -- string with the filename and location
+
+    Return: Logger object with handler
+    """
     handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
     formatter = logging.Formatter(logging.BASIC_FORMAT)
     handler.setFormatter(formatter)
