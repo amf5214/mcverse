c30c4ca46b73f6e7425b8309aad70f56fb841377
amf5214 <austinfraley.lee@gmail.com>
2023-11-28 14:46:01 -0500

    Added method to auto log
    Added method to auto log

--- src/logging_manager.py
+++ src/logging_manager.py
@@ -1,6 +1,7 @@
 import logging
 import logging.handlers
 import os
+import configparser
 
 def create_logger(filename):
     """Adds a handler to a new logger at the specified file location
@@ -12,7 +13,7 @@ def create_logger(filename):
 
     Return: Logger object with handler
     """
-    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
+    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='a')
     formatter = logging.Formatter('%(levelname)s:%(asctime)s:%(message)s')
     handler.setFormatter(formatter)
 
@@ -32,7 +33,7 @@ def get_root_logger(filename):
 
     Return: Logger object with handler
     """
-    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='w')
+    handler = logging.handlers.WatchedFileHandler(os.environ.get("LOGFILE", f'logging/{filename}.log'), mode='a')
     formatter = logging.Formatter(logging.BASIC_FORMAT)
     handler.setFormatter(formatter)
     root = logging.getLogger()
@@ -40,3 +41,25 @@ def get_root_logger(filename):
     root.addHandler(handler)
 
     return root
+
+def log_message(message, main=False, level='info'):
+    logging_active = False
+    app_config_file = 'appconfig.ini'
+    if os.path.isfile(app_config_file):
+        config = configparser.ConfigParser()
+        config.read(app_config_file)
+        logging_active = True if config['Main']['logging'] == "True" else False
+
+    logger = None
+
+    if logging_active and not main:
+        logger = create_logger('internal_operations')
+
+    elif logging_active and main:
+        logger = get_root_logger('root')
+
+    if logger != None and logging_active:
+        if level == 'error':
+            logger.error(message)
+        elif level == 'info':
+            logger.info(message)
